3d8c78512a377794aa19a009785308fc36f489d2
Daniel Alejandro <d4sf@outlook.com>
2022-07-03 00:37:36 -0600

    -crear modelo productos -crear tabla productos -crear controladores, etc

---
+++ database/migrations/2022_07_03_062526_update_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('brand', 100);
+            $table->string('name', 100);
+            $table->integer('price');
+            $table->string('type', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn(['brand', 'name', 'price', 'type']);
+        });
+    }
+};
