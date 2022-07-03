4bb8d36c9b4a68bd3333e182d2b7fa7334313bbb
Daniel Alejandro <d4sf@outlook.com>
2022-07-03 00:10:29 -0600

    -quitar tailwind -crear componentes nuevos -cambiar  nombre de welcome a home

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import TheNavbar from '@/Components/home/TheNavbar.vue';
+import TheCard from '@/Components/home/TheCard.vue';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+})
+</script>
+
+<template>
+
+    <Head title="Welcome" />
+    <TheNavbar />
+
+    <TheCard title="Impresora" />
+</template>
+
